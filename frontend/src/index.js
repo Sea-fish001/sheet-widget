8b8a87d514a7292e2b627081a7c30d72d2d188f1
Alexandr Filatov <78968475+var1etas@users.noreply.github.com>
2026-01-14 12:58:32 +0500

    Add error boundary wrapper for WidgetNode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
 import 'handsontable/dist/handsontable.full.min.css';
 
 export { default as WidgetNode } from './WidgetNode';
+export { WidgetNodeErrorBoundary, WidgetNodeSafe } from './WidgetNodeSafe';
